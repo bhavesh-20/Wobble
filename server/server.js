8f9910786ad14b569a422b06abb2dfb08708af84
Kaushik Rishi <rishi.cp01@gmail.com>
2022-04-05 10:30:03 +0530

    add morgan

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
-const express = require("express");
-const path = require("path");
-const connectDB = require("./config/db");
-const app = express();
+const express = require('express')
+const path = require('path')
+const connectDB = require('./config/db')
+const app = express()
+const morgan = require('morgan')
 
 // Connect to MongoDB
-connectDB();
+connectDB()
 
 const swaggerUi = require('swagger-ui-express')
-swaggerDocument = require('./swagger.json');
+swaggerDocument = require('./swagger.json')
 
 // Initialize middleware
-app.use(express.json());
+app.use(express.json())
+app.use(morgan('dev'))
 
 // Swagger UI
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Define routes
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/users", require("./routes/api/users"));
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/users', require('./routes/api/users'))
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
